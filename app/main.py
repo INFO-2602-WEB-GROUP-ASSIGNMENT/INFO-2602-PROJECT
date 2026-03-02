03e5b5c0b4adc8094efd4c269da119fa44998965
omatie <omatie.persad2004@gmail.com>
2026-03-02 17:48:52 -0400

    lab7 changes

--- app/main.py
+++ app/main.py
@@ -35,4 +35,7 @@ async def unauthorized_redirect_handler(request: Request, exc: Exception):
 
 
 if __name__ == "__main__":
+    print("Save")
+    print("Render")
     uvicorn.run("app.main:app", host=get_settings().app_host, port=get_settings().app_port, reload=get_settings().env.lower()!="production")
+
